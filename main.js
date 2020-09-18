ac7df3088755aa2763e2245ff6fea3f081ffd8d6
mklieb2 <marckliebert@gmail.com>
2020-09-18 19:13:57 +0000

    Final Project

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ healthcheck(callback) {
       * for the callback's errorMessage parameter.
       */
       this.emitOffline();
-      log.info('Service now adapter is offline {this.id}');
+      log.info('Service now adapter is offline User: ${this.props.auth.username} Adapter ID: ${this.id}, Error Detail: ${JSON.stringify(error)}');
       
       callbackError = error;
    } else {
@@ -148,7 +148,7 @@ healthcheck(callback) {
    */
   emitOffline() {
     this.emitStatus('OFFLINE');
-    log.warn('ServiceNow: Instance is unavailable.');
+    log.warn('ServiceNow: Instance is unavailable. ID: ${this.id}');
   }
 
   /**
@@ -160,7 +160,7 @@ healthcheck(callback) {
    */
   emitOnline() {
     this.emitStatus('ONLINE');
-    log.info('ServiceNow: Instance is available.');
+    log.info('ServiceNow: Instance is available. ID: ${this.id}');
   }
 
   /**
@@ -192,7 +192,25 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-     ServiceNowConnector.get(callback);
+     this.connector.get((data, error) => {
+        if (error) {
+          callback(data, error);} 
+        else {
+            if (data.hasOwnProperty('body')) {
+              var body_array = (JSON.parse(data.body));
+              var num_results = body_array.result.length;
+              var changeTicket = [];
+
+              for(var i = 0; i < num_results; i += 1) {
+                var result_array = (JSON.parse(data.body).result);
+                changeTicket.push({"change_ticket_number" : result_array[i].number, "active" : result_array[i].active, "priority" : result_array[i].priority,
+                                   "description" : result_array[i].description, "work_start" : result_array[i].work_start, "work_end" : result_array[i].work_end,
+                                   "change_ticket_key" : result_array[i].sys_id});
+              } 
+            callback(changeTicket, error); 
+            }
+          } 
+      });
   }
 
   /**
@@ -211,7 +229,21 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-     ServiceNowConnector.post(callback)
+     this.connector.post((data, error) => {
+          if (error) {
+            //console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
+            callback(data, error);} 
+        else {
+            if (data.hasOwnProperty('body')) {
+              var changeTicket = {};
+              var result_array = (JSON.parse(data.body).result);
+              changeTicket = ({"change_ticket_number" : result_array.number, "active" : result_array.active, "priority" : result_array.priority,
+                                   "description" : result_array.description, "work_start" : result_array.work_start, "work_end" : result_array.work_end,
+                                   "change_ticket_key" : result_array.sys_id});
+              callback(changeTicket, error); 
+            } 
+           }            
+        });    
   }
 }
 
